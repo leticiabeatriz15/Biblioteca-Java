21f5a33ca474d157ea06a12aa6901dbeb1114d0b
Leticia Beatriz <leticiabeatriz1508@gmail.com>
2025-03-12 14:16:27 -0300

    alteração no emprestimo.java

--- src/main/java/br/com/biblioteca/domain/emprestimo/Emprestimo.java
+++ src/main/java/br/com/biblioteca/domain/emprestimo/Emprestimo.java
@@ -2,23 +2,79 @@ package br.com.biblioteca.domain.emprestimo;
 
 import java.time.LocalDate;
 
+import br.com.biblioteca.domain.livro.Livro;
+import br.com.biblioteca.domain.usuario.Usuario;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Emprestimo {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-
     private LocalDate dataEmprestimo;
     private LocalDate dataDevolucao;
 
-    
+    @ManyToOne
+    private Usuario usuario;
 
+    @ManyToOne
+    private Livro livro;
 
+    public Emprestimo(){
+        
+    }
 
+    public Emprestimo(Long id, LocalDate dataEmprestimo, LocalDate dataDevolucao, Usuario usuario, Livro livro) {
+        this.id = id;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+        this.usuario = usuario;
+        this.livro = livro;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(LocalDate dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public LocalDate getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(LocalDate dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
     
 }
