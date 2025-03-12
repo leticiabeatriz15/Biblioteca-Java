23ae9c597798ff737368cef16c14eed750f70a08
Emanuelle <capic.2024118tads0027@aluno.ifpi.edu.br>
2025-03-12 16:31:54 -0300

    EmprestimoDto

--- src/main/java/br/com/biblioteca/domain/emprestimo/EmprestimoDto.java
+++ src/main/java/br/com/biblioteca/domain/emprestimo/EmprestimoDto.java
@@ -2,8 +2,10 @@ package br.com.biblioteca.domain.emprestimo;
 
 import java.time.LocalDate;
 
+import br.com.biblioteca.domain.livro.Livro;
+import br.com.biblioteca.domain.usuario.Usuario;
 import jakarta.validation.constraints.NotBlank;
 
-public record EmprestimoDto (@NotBlank LocalDate dataEmprestimo, @NotBlank LocalDate dataDevolucao, @NotBlank String status){
+public record EmprestimoDto (@NotBlank Usuario usuario, @NotBlank Livro livro, @NotBlank LocalDate dataEmprestimo, @NotBlank LocalDate dataDevolucao, @NotBlank String status){
     
 }
