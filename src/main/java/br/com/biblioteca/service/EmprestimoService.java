06fa51e7169d9f3abdb97d3f5e99262ee182ff3c
Emanuelle <capic.2024118tads0027@aluno.ifpi.edu.br>
2025-03-12 23:39:08 -0300

    EmprestimoService

--- src/main/java/br/com/biblioteca/service/EmprestimoService.java
+++ src/main/java/br/com/biblioteca/service/EmprestimoService.java
@@ -1,9 +1,15 @@
 package br.com.biblioteca.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.biblioteca.domain.emprestimo.EmprestimoRepository;
 
 @Service
 public class EmprestimoService {
     
+    @Autowired
+    private EmprestimoRepository emprestimoRepository;
+
     
 }
